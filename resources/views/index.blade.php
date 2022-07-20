44c92ca4163684d17743c86663414caafc5fa02b
ari ys <ari13yustias@gmail.com>
2022-07-20 16:34:33 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,34 +18,40 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg fixed-bottom navbar-light" id="bot-menu">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#home"> <span class="material-symbols-outlined">home</span>
-                        <div class="dtext">Home</div>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#story"><span class="material-symbols-outlined">favorite</span>
-                        <div class="dtext">Story</div>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#gallery"><span class="material-symbols-outlined">perm_media</span>
-                        <div class="dtext">Gallery</div>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#guestBook"><span class="material-symbols-outlined">menu_book</span>
-                        <div class="dtext">Guest Book</div>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <div class="floating_button">
+            <img src="https://smallimg.pngkey.com/png/small/124-1243758_gift-box-with-lace-free-vectors-logos.png" alt="">
+            <button class="btn">Kirim Hadiah</button>
+        </div>
+        <div id="navbar_master">
+            <nav class="navbar navbar-expand-lg fixed-bottom navbar-light" id="bot-menu">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#home"> <span class="material-symbols-outlined">home</span>
+                            <div class="dtext">Home</div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#story"><span class="material-symbols-outlined">favorite</span>
+                            <div class="dtext">Our Story</div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#gallery"><span class="material-symbols-outlined">perm_media</span>
+                            <div class="dtext">Gallery</div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#guestbook"><span class="material-symbols-outlined">menu_book</span>
+                            <div class="dtext">Guest Book</div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 
     <main>
-        <section class="section-story">
+        <section class="section-home" id="home">
             <div class="container">
                 <img style="position: absolute; width:20%; left:10px; bottom:10px; transform:scaleX(-1);" src="{{asset('icons/golden_flower2.png')}}" alt="">
                 <img style="position: absolute; width:20%; right:10px; bottom:10px;" src="{{asset('icons/golden_flower2.png')}}" alt="">
@@ -58,34 +64,117 @@
                 </div>
                 <div class="word mt-2">
                     <h3>The Wedding Of</h3>
-                    <h1>ARI YUSTIA</h1>
+                    <h1>Ari Yustia</h1>
                     <h2>SAPUTRA</h2>
                     <h4>And</h4>
                     <h1>FEBYOLA</h1>
                     <h2>AGATHA</h2>
                     <h4>26 AUG 2022 | ON EARTH NOT MARS</h4>
                 </div>
-
-
             </div>
         </section>
-        <section class="section-story-2">
+        <section class="section-story" id="story">
             <div class="container">
-                <p>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau
-                    typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang
-                    cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku
-                    contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf
-                    elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya
-                    lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya
-                    perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.Sudah
-                    merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia
-                    melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki
-                    penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti "Bagian isi disini, bagian isi
-                    disini", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing
-                    dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian
-                    terhadap kalimat "Lorem Ipsum" akan berujung pada banyak situs web yang masih dalam tahap
-                    pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja,
-                    kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)</p>
+                <div class="story-header">
+                    <img src="{{asset('icons/black_flower.png')}}" alt="">
+                    <h1>Our Story</h1>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Pertemuan</h5>
+                      <h6 class="card-subtitle mb-2">2011</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Pertemanan</h5>
+                      <h6 class="card-subtitle mb-2">2012</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Persahabatan</h5>
+                      <h6 class="card-subtitle mb-2">2014</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Percintaan</h5>
+                      <h6 class="card-subtitle mb-2">2022</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Pernikahan</h5>
+                      <h6 class="card-subtitle mb-2">2022</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Permulaan Perjalanan Bersama</h5>
+                      <h6 class="card-subtitle mb-2">2022</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-gallery" id="gallery">
+            <div class="container">
+                <div class="gallery-header">
+                    <img src="{{asset('icons/golden_flower3.png')}}" alt="">
+                    <h1>Gallery</h1>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <img src="{{asset('img/us.png')}}" alt="">
+                  </div>
+                  <div class="col">
+                    <img src="{{asset('img/us.png')}}" alt="">
+                  </div>
+                  <div class="col">
+                    <img src="{{asset('img/us.png')}}" alt="">
+                  </div>
+                </div>
+              </div>
+        </section>
+        <section class="section-guestbook" id="guestbook">
+            <div class="container">
+                <div class="guestbook_header">
+                    <h1>Guest Book</h1>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">Name</h5>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                          </div>
+                        <h5 class="card-title">Message</h5>
+                        <div class="input-group">
+                            <textarea class="form-control" aria-label="With textarea"></textarea>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ujang</h5>
+                        <p class="card-text">Selamattt .....</p>
+                        <p href="#" class="card-link">20-08-2020</p>
+                        <hr style="color: white">
+
+                        <h5 class="card-title">Ujang</h5>
+                        <p class="card-text">Selamattt .....</p>
+                        <p href="#" class="card-link">20-08-2020</p>
+                        <hr style="color: white">
+
+                        <h5 class="card-title">Ujang</h5>
+                        <p class="card-text">Selamattt .....</p>
+                        <p href="#" class="card-link">20-08-2020</p>
+                        <hr style="color: white">
+                    </div>
+                  </div>
             </div>
         </section>
     </main>
@@ -99,13 +188,17 @@
     <script src="{{asset('js/popper.min.js')}}"></script>
 
     <script>
-        let love = document.getElementById('love');
-        window.addEventListener('scroll', function () {
-            let value = window.scrollY;
-            console.log(value);
-            love.style.top = value * 1.05 + 'px';
-        })
-    </script>
+        // Add active class to the current button (highlight it)
+        var header = document.getElementById("navbar_master");
+        var btns = header.getElementsByClassName("nav-link");
+        for (var i = 0; i < btns.length; i++) {
+          btns[i].addEventListener("click", function() {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+          });
+        }
+        </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
 
 Route::get('/', [IndexController::class, 'index']);
